e756fc39673c4f846dab3c4b338709a8f05918d9
svartmc <60356241+svartmc@users.noreply.github.com>
2020-07-02 17:25:42 +0200

    Add files via upload

---
+++ fakelight.js
@@ -0,0 +1,35 @@
+function fakeLight( mesh ) {
+
+	var geometry = mesh.geometry;
+
+	var count = geometry.attributes.position.count;
+
+	var positions = geometry.attributes.position;
+
+	geometry.setAttribute( 'color', new THREE.BufferAttribute( new Float32Array( count * 3 ), 3 ) );
+
+	var color = new THREE.Color();
+
+	var colors = geometry.attributes.color;
+
+	var normals = geometry.attributes.normal;
+
+	var greyValue = new THREE.Color( 0xe9e9e9 );
+
+	// vertex colors
+
+	for ( var i = 0; i < count; i ++ ) {
+
+		var norms = normals.getX( i ) + normals.getZ( i ) / 4 + normals.getY( i ) * 1.2;
+
+		var posX = positions.getX( i );
+		var posY = positions.getY( i );
+		var posZ = positions.getZ( i );
+
+		colors.setXYZ( i, greyValue.r + norms/20, greyValue.g + norms/20, greyValue.b + norms/20 );
+
+	}
+
+	mesh.material.vertexColors = THREE.VertexColors;
+
+}
